3c2a4e055517e8e4437a5f81a2d10c422a50fdbc
hgkang2 <hgkang2hank@naver.com>
2025-01-17 11:12:58 +0900

    캐릭터

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class PlayerCtrl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log ("HelloWorld");
+        
     }
 
     // Update is called once per frame
