6a4861934e9690e7fd41d116b40d6e49cd4ad0d9
hgkang2 <hgkang2hank@naver.com>
2025-02-06 14:51:06 +0900

    바렐,총알이펙트

--- Assets/02.Scripts/RemoveBullet.cs
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
+    public GameObject sparkEffect;
     // 충돌이 시작할 때 발생하는 이벤트
     void OnCollisionEnter(Collision coll)
     {
         //tag값 비교
         if(coll.collider.CompareTag("BULLET"))
         {
-            //충돌한 gameobject 삭제
+            ContactPoint cp = coll.GetContact(0);
+            Quaternion rot = Quaternion.LookRotation(-cp.normal);
+            GameObject spark = Instantiate(sparkEffect,cp.point,rot);
+            Destroy(spark, 0.5f);
             Destroy(coll.gameObject);
         }
     }
