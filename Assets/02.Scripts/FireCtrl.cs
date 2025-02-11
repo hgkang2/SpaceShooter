9c956a7fff7fac233b6bc8a35733532145ea343f
hgkang2 <hgkang2hank@naver.com>
2025-02-11 13:01:13 +0900

    총알사운드

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//반드시 필요한 컴포턴트를 삭제 안되도록 하는 어트리트뷰
+[RequireComponent(typeof(AudioSource))]
 public class FireCtrl : MonoBehaviour
 {
     public GameObject bullet;
     public Transform firePos;
-    // Start is called before the first frame update
+    public AudioClip firesfx;
+    private new AudioSource audio;
     void Start()
     {
-        
+        audio = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -23,5 +25,6 @@ public class FireCtrl : MonoBehaviour
     void Fire()
     {
         Instantiate(bullet, firePos.position, firePos.rotation);
+        audio.PlayOneShot(firesfx, 1.0f);
     }
 }
