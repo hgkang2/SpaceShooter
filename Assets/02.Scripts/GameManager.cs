7cff7a427d82944b88a793ce94c73d17cc6d304c
hgkang2 <hgkang2hank@naver.com>
2025-03-11 13:01:20 +0900

    ..

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -1,23 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
-using UnityEditor.ShortcutManagement;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     //public Transform[] points;
     public List<Transform> points = new List<Transform>();
+    public GameObject monster;
+    public float createTime = 3.0f;
+    private bool isGameOver;
+    public bool IsGameOver
+    {
+        get{return isGameOver;}
+        set{
+            isGameOver = value;
+            if (isGameOver)
+            {
+                CancelInvoke("Create<onster");
+            }
+        }
+    }
     void Start()
     {
-       // Transform spawnPointGroup = GameObject.Find("SpawnPointGroup")?.transform;
-       // points = spawnPointGroup?.GetComponentsInChildren<Transform>();
-       Transform spawnPointGroup = GameObject.Find("SpawnPointGroup")?.transform;
-       //points = spawnPointGroup?.GetComponentsInChildren<Transform>();
-       //spawnPointGroup?.GetComponentsInChildren<Transform>(points);
-       foreach (Transform point in spawnPointGroup)
-       {
-        points.Add(point);
-       }
+        //GameObject spawnPointGroupObject = GameObject.Find("SpawnPointGroup");
+        //if (spawnPointGroupObject != null)
+        //{
+        //    Transform spawnPointGroup = spawnPointGroupObject.GetComponent<Transform>();
+        //    points = spawnPointGroup.GetComponentsInChildren<Transform>();
+        //}
+        Transform spawnPointGroup = GameObject.Find("SpawnPointGroup")?.transform;
+
+        //points = spawnPointGroup?.GetComponentsInChildren<Transform>();
+        //spawnPointGroup?.GetComponentsInChildren<Transform>(points);
+        foreach (Transform point in spawnPointGroup)
+        {
+            points.Add(point);
+        }
+        InvokeRepeating("CreateMonster",2.0f,createTime);
     }
 }
